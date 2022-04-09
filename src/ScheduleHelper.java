c3449c95d88716c768a6cd52bafc381991efc2fa
Venn Reddy <vennreddy490@gmail.com>
2022-04-09 15:47:25 -0400

    Added basic calendar components

--- src/ScheduleHelper.java
+++ src/ScheduleHelper.java
@@ -1,25 +1,37 @@
+import java.sql.Time;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-
 
 public class ScheduleHelper extends Application {
 
+    @Override
     public void start(Stage stage) {
 
+        // Adding days and coloumns
         HBox root = new HBox();
-        Scene scene = new Scene(root);
+        TimeSlot sunday = new TimeSlot("Sunday");
+        TimeSlot monday = new TimeSlot("Monday");
+        TimeSlot tuesday = new TimeSlot("Tuesday");
+        TimeSlot wednesday = new TimeSlot("Wednesday");
+        TimeSlot thursday = new TimeSlot("Thursday");
+        TimeSlot friday = new TimeSlot("Friday");
+        TimeSlot saturday = new TimeSlot("Saturday");
+        root.getChildren().addAll(sunday, monday, tuesday, wednesday, thursday, friday, saturday);
 
+        
+
+        // Initializing the Scene and displaying it
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        stage.setTitle("Schedule Helper");
 
-    }
-
-    public static void main(String[] args) {
-        System.out.println(ScheduleCalc.bob);
-        ScheduleHelper.launch();
     }
 
 }
